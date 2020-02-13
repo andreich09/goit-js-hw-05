5d42a27a5cd3418a7ae564847c7343bf286311d5
Dmytriy Kravhenko <andreich1@bigmir.net>
2020-02-13 21:39:43 +0200

    added home work 05

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+    getItems = function() {
+        return this.items;
+    };
+
+    addItem = function(item) {
+        this.items.push(item);
+        return this.items;
+    };
+
+    removeItem = function(item) {
+        this.items.splice(this.items.indexOf(item), 1);
+        return this.items;
+    };
+}
+
+const storage = new Storage([ 'Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор' ]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
